a06489b8cb7cc91109fc479da607204c48d60c56
Alex Ionescu <alxi.2001@gmail.com>
2021-09-19 03:13:24 +0300

    Update prelude

--- src/Language/Dynasty/Runtime/Prelude.hs
+++ src/Language/Dynasty/Runtime/Prelude.hs
@@ -4,10 +4,13 @@ import Control.Category((>>>))
 import Data.Map.Lazy(Map)
 import qualified Data.Map.Lazy as M
 import Data.Text(Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
 import Debug.Trace(trace)
 
 import Language.Dynasty.Frontend.Syntax
 import Language.Dynasty.Runtime.Val
+import Utils
 
 type Env = Map Ident Val
 
@@ -37,22 +40,24 @@ prelude args =
   , ("pure", toVal $ pure @IO @Val)
   , ("*>", toVal $ (*>) @IO @Val @Val)
   , (">>=", toVal $ (>>=) @IO @Val @Val)
-  , ("getLine", toVal getLine)
+  , ("getLine", toVal T.IO.getLine)
   , ("print", toVal $ print @Val)
-  , ("putStrLn", toVal putStrLn)
-  , ("show", toVal $ show @Val)
-  , ("read", toVal $ read @Val)
+  , ("putStrLn", toVal T.IO.putStrLn)
+  , ("show", toVal $ showT @Val)
+  , ("read", toVal $ readT @Val)
   , ("==", toVal $ (==) @Val)
   , (".", toVal $ (.) @Val @Val @Val)
   , ("&", toVal mergeRecords)
-  , ("trace", toVal $ trace @Val)
+  , ("trace", toVal $ trace @Val . T.unpack)
   , ("typeOf", toVal typeOf)
   , ("<", toVal $ (<) @Integer)
-  , ("readFile", toVal readFile)
+  , ("readFile", toVal $ T.IO.readFile . T.unpack)
   , ("getChar", toVal getChar)
   , ("putChar", toVal putChar)
   , ("getArgs", toVal $ pure @IO args)
   , ("charToNum", toVal $ toInteger . fromEnum @Char)
   , ("numToChar", toVal $ toEnum @Char . fromInteger)
   , (";", toVal $ (>>>) @(->) @Val @Val @Val)
+  , ("explode", toVal T.unpack)
+  , ("implode", toVal T.pack)
   ]

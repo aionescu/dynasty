bfd5b80a426f59f0dd23a1f71b3b8ce86931855d
Alex Ionescu <alxi.2001@gmail.com>
2022-09-01 02:20:47 +0200

    Improve error messages for invalid project dir

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,9 @@ import Control.Monad(join)
 import Data.Function((&))
 import Data.Functor((<&>))
 import Data.Maybe(fromMaybe)
+import Data.Text(Text)
 import Data.Text.IO qualified as T
-import System.Directory(listDirectory, doesFileExist)
+import System.Directory(listDirectory, doesDirectoryExist, doesFileExist)
 import System.Exit(exitFailure)
 import System.FilePath((</>), isExtensionOf)
 
@@ -15,7 +16,10 @@ import Language.Dynasty.Simplify(simplify)
 import Language.Dynasty.Codegen(compile)
 import Opts(Opts(..), getOpts)
 import Paths_dynasty(getDataDir)
-import Utils(foldMapM)
+import Utils(foldMapM, showT)
+
+showErr :: Text -> IO a
+showErr e = T.putStrLn e *> exitFailure
 
 getDyFiles :: FilePath -> IO [FilePath]
 getDyFiles path =
@@ -23,12 +27,21 @@ getDyFiles path =
     True -> pure [path | "dy" `isExtensionOf` path]
     _ -> listDirectory path >>= foldMapM (getDyFiles . (path </>))
 
+parseDir :: Text -> FilePath -> IO [FilePath]
+parseDir label path =
+  doesDirectoryExist path >>= \case
+    True -> getDyFiles path
+    _ ->
+      doesFileExist path >>= \case
+        True -> showErr $ "Error: " <> label <> " must be a directory. " <> showT path <> " is a file."
+        _ -> showErr $ "Error: " <> label <> " " <> showT path <> " does not exist."
+
 run :: Opts -> IO ()
 run Opts{..} = do
   coreDir <- (</> "core") <$> getDataDir
   runtime <- T.readFile $ coreDir </> "runtime.js"
-  coreFiles <- getDyFiles coreDir
-  userFiles <- getDyFiles optsPath
+  coreFiles <- parseDir "Core dir" coreDir
+  userFiles <- parseDir "Project dir" optsPath
   modules <- traverse (join $ curry $ traverse T.readFile) $ coreFiles <> userFiles
 
   modules
@@ -36,9 +49,7 @@ run Opts{..} = do
     >>= validate
     <&> simplify
     <&> compile runtime
-    & either
-      (\e -> T.putStrLn e *> exitFailure)
-      (T.writeFile $ fromMaybe (optsPath </> "main.js") optsOutPath)
+    & either showErr (T.writeFile $ fromMaybe (optsPath </> "main.js") optsOutPath)
 
 main :: IO ()
 main = getOpts >>= run

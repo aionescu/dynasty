2ef0de1dc54e710dfb40e82bf6549a3261899463
Alex Ionescu <alxi.2001@gmail.com>
2022-09-10 06:24:24 +0200

    Update `core` and examples

--- core/runtime.js
+++ core/runtime.js
@@ -8,7 +8,7 @@ const show=(a,p)=>{
   else if(Object.keys(a).length===0)r="{ }";
   else if(!a.$)r="{ "+Object.entries(a).map(f=>f[0].slice(1).replace("_","'")+" = "+show(e(f[1]),false)).join(", ")+" }";
   else if(a.$==="Tuple"&&!(a.$0&&!a.$1))r="("+Object.values(a).slice(1).map(f=>show(e(f),p)).join(", ")+")";
-  else if(a.$==="Nil"&&!a.$0)r="[]";
+  else if(!a.$0)r=a.$==="Nil"?"[]":a.$;
   else if(!a.$.match(/[A-Z]/)&&a.$1&&!a.$2)q=true,r=show(e(a.$0),true)+" "+a.$+" "+show(e(a.$1),true);
   else q=true,r=Object.values(a).slice(1).reduce((s,f)=>s+" "+show(e(f),true),a.$.match(/[A-Z]/)?a.$:"("+a.$+")");
   if(p&&q)r="("+r+")";return r;
